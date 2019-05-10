7b15f769370c590c14579dc3339e2edec8a840dd
mh-rafi <rafi.bogra@gmail.com>
2019-05-11 00:36:29 +0600

    updated doc

--- docs/assets/js/doc.js
+++ docs/assets/js/doc.js
@@ -1,31 +1,34 @@
 $(document).ready(function() {
-
   // hilight
-  var $codes = $('.code');
-  hljs.configure({useBR: true});
+  var $codes = $(".code");
+  hljs.configure({ useBR: true });
 
   $codes.each(function(e) {
     var $code = $(this);
-    var code = $code.data('code');
+    var code = $code.data("code");
     var highlighted = hljs.highlightAuto(code);
-    $code.addClass('hljs xml');
-    $code.html(hljs.fixMarkup(highlighted.value))
-  })
+    $code.addClass("hljs xml");
+    $code.html(hljs.fixMarkup(highlighted.value));
+  });
 
   // copy to clipboard
-  var clipboard = new ClipboardJS('.btn-clipboard', {
-    target: function (trigger) {
-      return trigger.parentNode.nextElementSibling
+  var clipboard = new ClipboardJS(".btn-clipboard", {
+    target: function(trigger) {
+      return trigger.parentNode.nextElementSibling;
     }
-  })
+  });
 
-  clipboard.on('success', function (e) {
-    
-  })
+  clipboard.on("success", function(e) {});
 
-  clipboard.on('error', function (e) {
-    
-  })
-})
+  clipboard.on("error", function(e) {});
 
-hljs.initHighlightingOnLoad();
+  // Load Sidebar
+  $.get("template-parts/sidebar-nav.html", function(data) {
+    var $sidebar = $(".doc-content__sidebar").html(data);
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    $sidebar.find('.sidebar__list [href="'+ page +'"]').addClass("active");
+  });
+});
+
+// hljs.initHighlightingOnLoad();
